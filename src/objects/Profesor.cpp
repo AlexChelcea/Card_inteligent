24bfb031f53dc865d8e6a055ee66bd5b4c3e0cf1
AlexChelcea <alex.chelcea79@gmail.com>
2026-04-01 20:19:13 +0300

    Am creat clasa profesor

---
+++ src/objects/Profesor.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Profesor.h"
+using namespace std;
+
+void Profesor::afisare(){
+    cout << "Profesor:" << FirstName << " " << LastName << endl;
+}
---
+++ src/objects/Profesor.h
@@ -0,0 +1,12 @@
+#ifndef PROFESOR_H
+#define PROFESOR_H
+#include <string>
+
+class Profesor{
+    public:
+    std::string LastName;
+    std::string FirstName;
+    void afisare();
+};
+
+#endif;
